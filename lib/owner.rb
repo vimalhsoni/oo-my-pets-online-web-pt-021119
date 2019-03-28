2be13ebdce763c262677d452b73d4bc518878f25
Vimal Soni <vimal.h.soni@gmail.com>
2019-03-28 04:09:50 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -5,10 +5,10 @@ class Owner
   attr_accessor :name
   attr_reader :species
 
-  def initialize(name, species)
+  def initialize(name)
     @name = name
-    @species = species
     @@all << self
+    @species = species
   end
 
   def self.all
