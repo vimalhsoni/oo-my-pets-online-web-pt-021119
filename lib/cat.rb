8b29f7d2ede34c20862a64cf986ecfb46e233380
Vimal Soni <vimal.h.soni@gmail.com>
2019-03-28 03:24:08 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,13 @@
 class Cat
   # code goes here
+  #attr_accessor needed as writer/reader for attributes
+  attr_reader :name
+  attr_accessor :mood
+  #need to initialize a Cat
+  def initialize(name, mood = "nervous")
+    @name = name
+    @mood = mood
+  end
+
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,13 @@
 class Owner
   # code goes here
+@@all = []
+
+def initialize
+  @@all << self
+end
+
+def self.all
+@@all
+end
+
 end
