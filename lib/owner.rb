e45c054742856c92fc05935f857057148af8a262
Vimal Soni <vimal.h.soni@gmail.com>
2019-03-28 04:25:55 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -40,7 +40,28 @@ class Owner
   end
 
   def walk_dogs
-    Dog.mood = "happy"
+    pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end
   end
+
+  def play_with_cats
+    pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def feed_fish
+    pets[:fish].each do |fish|
+      fish.mood = "happy"
+    end
+  end
+
+  def sell_pets
+  end
+
+  def list_pets
+  end
+  
   
 end
